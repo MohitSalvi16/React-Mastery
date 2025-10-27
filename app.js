eec5456f38df23d71777c5f72a294ea8f2710665
MohitSalvi16 <72203459+MohitSalvi16@users.noreply.github.com>
2025-10-27 07:40:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+   const parent=React.createElement("div",
+      {id: "Parent"},
+      [React.createElement("div",{id: "child"}, 
+         [React.createElement("h1",{}, "I'm a h1 tag"),
+            React.createElement("h2",{}, "I'm a h2 tag")])],
+
+      [React.createElement("div",{id: "child"}, 
+         [React.createElement("h1",{}, "I'm a h1 tag"),
+            React.createElement("h2",{}, "I'm a h2 tag")]),])
+   
+   const heading=React.createElement("h1", {id: "heading"},"This is a React test");
+
+   const root=ReactDOM.createRoot(document.getElementById("root"));
+
+   console.log(parent);
+   root.render(parent);

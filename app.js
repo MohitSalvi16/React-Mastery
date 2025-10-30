33092781d85c48a5d409cfe50b622dcbd7d9e81c
MohitSalvi16 <72203459+MohitSalvi16@users.noreply.github.com>
2025-10-30 23:55:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+   import React from "react";
+   import ReactDOM from "react-dom/client";
+   
    const parent=React.createElement("div",
       {id: "Parent"},
       [React.createElement("div",{id: "child"}, 
-         [React.createElement("h1",{}, "I'm a h1 tag"),
+         [React.createElement("h1",{}, "this is changed line"),
             React.createElement("h2",{}, "I'm a h2 tag")])],
 
       [React.createElement("div",{id: "child"}, 
          [React.createElement("h1",{}, "I'm a h1 tag"),
             React.createElement("h2",{}, "I'm a h2 tag")]),])
    
-   const heading=React.createElement("h1", {id: "heading"},"This is a React test");
+   //const heading=React.createElement("h1", {id: "heading"},"This is a React test");
 
    const root=ReactDOM.createRoot(document.getElementById("root"));
 
